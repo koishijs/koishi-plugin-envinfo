32775de490a228be3f0389b809e5e92c5d086061
Shigma <shigma10826@gmail.com>
2022-12-18 23:15:59 +0800

    feat: support copy to clipboard

--- client/status.vue
+++ client/status.vue
@@ -1,5 +1,5 @@
 <template>
-  <k-status class="version">
+  <k-status class="version" @click="copyInfo">
     Koishi v{{ store.envinfo.koishi.Core }}
     <template #tooltip>
       <div class="section" v-for="(data, key) in store.envinfo">
@@ -14,7 +14,16 @@
 
 <script lang="ts" setup>
 
-import { capitalize, store } from '@koishijs/client'
+import { capitalize, message, store } from '@koishijs/client'
+import { copyToClipboard } from './utils'
+
+async function copyInfo() {
+  const text = Object.entries(store.envinfo).map(([key, data]) => {
+    return `${capitalize(key)}:\n` + Object.entries(data).map(([key, value]) => `    ${key}: ${value}`).join('\n')
+  }).join('\n\n')
+  await copyToClipboard(text)
+  message.success('已复制环境信息！')
+}
 
 </script>
 
